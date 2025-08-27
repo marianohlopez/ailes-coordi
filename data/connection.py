40c1f6fb8d33a2450557ec8616453635b0fc6ae5
mlopez <ml.3012@gmail.com>
2025-08-27 15:32:10 -0300

    map

---
+++ data/connection.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()   
+
+def get_connection():
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASS")
+    host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
+    dbname = os.getenv("DB_NAME")
+
+    connection_string = f"mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}"
+    engine = create_engine(connection_string, pool_pre_ping=True)
+
+    return engine
---
+++ logic/filters.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from data.queries import q_filter_os
+
+# Filtro selector de OS
+
+def filtro_os(conn):
+
+  df_os = q_filter_os(conn)
+
+  obras_sociales = ['Todas las os'] + list(df_os['os_nombre'].unique())
+
+  # Selector en Streamlit
+  selected_os = st.selectbox("Seleccione una obra social:", obras_sociales)
+
+  # Condición de os en la consulta
+  os_condition = ""
+  if selected_os != "Todas las os":
+      os_condition = f"AND o.os_nombre = '{selected_os}'"
+      
+  return os_condition
